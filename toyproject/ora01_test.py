14682b22b34544008fd1d34cb09897bedb2c8f16
BBiT808 <witchsparadise11003311@gmail.com>
2025-03-20 12:31:46 +0900

    20250320 sql 8번째 수업
    qtdesigner.... UI 만들기... 개어렵다..

--- toyproject/ora01_test.py
+++ toyproject/ora01_test.py
@@ -5,7 +5,7 @@
 import cx_Oracle as oci
 
 # DB연결 설정변수 선언
-sid 'XE'
+sid = 'XE'
 host = '127.0.0.1' # = localhost와 동일
 # ex) DB서버가 외부에 있다면 ; oracle.pknuprd.net(오라클 서버 주소 적기) or 211.12.119.45(아이피 서버주소 적기)
 port = 1521
@@ -21,6 +21,9 @@ query = 'SELECT * FROM Students' # 파이썬에서 쿼리 호출 시엔 ;(세미
 cursor.execute(query)  # 위의 쿼리를 실행해줌
 
 # 불러온 데이터 처리
+for i, item in enumerate(cursor, start=1):
+    print(item)
+
 cursor.close()
 conn.close()
 # DB는 연결하면 마지막 close(), 파일은 오픈하면 마지막에 닫아줘야 함!!(꼭 연결을 끊어주자~ : 안 닫으면 다른 사람들이 못 쓴대!!)
